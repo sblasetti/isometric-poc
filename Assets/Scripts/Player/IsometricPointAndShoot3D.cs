9afa7b873fbc0dbab56530056aac6e838c6fcec8
Sebastian Blasetti <sblasetti@gmail.com>
2022-08-19 00:03:20 -0300

    Add aiming behavior

---
+++ Assets/Scripts/Player/IsometricPointAndShoot3D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class IsometricPointAndShoot3D : MonoBehaviour
+{
+    public float speed = 6f;
+    public KeyCode lookAtPointerKey = KeyCode.Mouse1;
+    public float turnSmoothTime = 0.05f;
+
+    private bool isPointing;
+
+    void Update()
+    {
+        isPointing = Input.GetKey(lookAtPointerKey);
+
+        if (isPointing)
+        {
+            // Character rotation considering the camera
+            var playerPlane = new Plane(Vector3.up, transform.position);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitDistance;
+            if (playerPlane.Raycast(ray, out hitDistance))
+            {
+                var targetPoint = ray.GetPoint(hitDistance);
+                var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public bool IsPointing()
+    {
+        return isPointing;
+    }
+}
--- Assets/Scripts/Player/IsometricWalk3D.cs
+++ Assets/Scripts/Player/IsometricWalk3D.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class IsometricWalk3D : MonoBehaviour
 {
     public CharacterController characterController;
     public Transform cameraTransform;
     public float speed = 6f;
-    public bool lookAtPointer = false;
-    public KeyCode lookAtPointerKey = KeyCode.LeftShift;
 
     public float turnSmoothTime = 0.05f;
     float turnSmoothVelocity;
+    IsometricPointAndShoot3D pointAndShootBehavior;
+
+    void Start()
+    {
+        pointAndShootBehavior = transform.GetComponent<IsometricPointAndShoot3D>();
+    }
 
     void Update()
     {
@@ -21,34 +23,24 @@ public class IsometricWalk3D : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            var isJustWalking = pointAndShootBehavior == null || !pointAndShootBehavior.IsPointing();
+
             // Character rotation considering the camera
             float targetDirectionAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
-            float directionAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetDirectionAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = GetPlayerRotation(directionAngle);
+
+            // Only rotate the character based on movement if not pointing
+            if (isJustWalking)
+            {
+                float directionAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetDirectionAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, directionAngle, 0f);
+            }
 
             // Take into consideration the camera's position
             Vector3 movementDirection = Quaternion.Euler(0f, targetDirectionAngle, 0f) * Vector3.forward;
 
             // Character movement
-            characterController.Move(movementDirection.normalized * speed * Time.deltaTime);
+            var movementSpeed = isJustWalking ? speed : pointAndShootBehavior.speed;
+            characterController.Move(movementDirection.normalized * movementSpeed * Time.deltaTime);
         }
-    }
-
-    private Quaternion GetPlayerRotation(float directionAngle)
-    {
-        if (lookAtPointer && Input.GetKey(lookAtPointerKey))
-        {
-            var playerPlane = new Plane(Vector3.up, transform.position);
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float hitDistance;
-            if (playerPlane.Raycast(ray, out hitDistance))
-            {
-                var targetPoint = ray.GetPoint(hitDistance);
-                var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-                return Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            }
-        }
-
-        return Quaternion.Euler(0f, directionAngle, 0f);
     }
 }
