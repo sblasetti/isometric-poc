9c87504e904b02f10b9a2b7fb2990f780d7743f9
Sebastian Blasetti <sblasetti@gmail.com>
2022-08-19 21:21:43 -0300

    Camera follows player

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    private Vector3 cameraOffset;
+
+    void Start()
+    {
+        cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = PlayerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, SmoothFactor);
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Setting isometric camera](https://www.youtube.com/watch?v=4HpC--2iowE)
 - [Player movement](https://www.youtube.com/watch?v=4HpC--2iowE)
 - [Rotate player to look at mouse pointer](https://www.youtube.com/watch?v=4HpC--2iowE)
+- [Camera to follow the player](https://www.youtube.com/watch?v=urNrY7FgMao)
